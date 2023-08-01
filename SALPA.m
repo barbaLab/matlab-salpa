e4c8e4a35499d2c13a5437dff60c658e13867753
Francesco Negri <francesco.negri@outlook.com>
2023-08-01 11:11:37 -0500

    Add support for all python-salpa arguments

--- SALPA.m
+++ SALPA.m
@@ -1,24 +1,61 @@
-function sig = SALPA(sig, p)
+function output = salpa(signal, tau, varargin)
+    validNumPosCheck = @(x) isnumeric(x) && (x >= 0);
+    
+    parser = inputParser();
+    addRequired(parser, 'signal', @isnumeric);
+    addRequired(parser, 'tau', @(x) isnumeric(x) && all(x > 0));
+    addOptional(parser, 'stimIdxs', 1, @(x) isnumeric(x) && all(x > 0));
+    addParameter(parser, 'rail1', [], @isnumeric);
+    addParameter(parser, 'rail2', [], @isnumeric);
+    addParameter(parser, 'thresh', [], @isnumeric);
+    addParameter(parser, 'tBlankDepeg', [], validNumPosCheck);
+    addParameter(parser, 'tAhead', [], @validNumPosCheck);
+    addParameter(parser, 'tChi2', [], @validNumPosCheck);
+    addParameter(parser, 'tForcePeg', [], @validNumPosCheck);
+    addParameter(parser, 'hasNan', false, @islogical);
 
-    returnColumn = false;
-    if iscolumn(sig)
-        sig = sig';
-        returnColumn = true;
+    parse(parser, signal, tau, varargin{:});
+
+    signal = parser.Results.signal;
+    stimIdxs = parser.Results.stimIdxs;
+    tau = parser.Results.tau;
+    rail1 = parser.Results.rail1;
+    rail2 = parser.Results.rail2;
+    thresh = parser.Results.thresh;
+    tBlankDepeg = parser.Results.tBlankDepeg;
+    tAhead = parser.Results.tAhead;
+    tChi2 = parser.Results.tChi2;
+    tForcePeg = parser.Results.tForcePeg;
+    hasNan = parser.Results.hasNan;
+
+    if iscolumn(signal)
+        output = signal';
+    else
+        output = signal;
     end
 
-    p.StimI = [1, p.StimI];
-    artifactNSamples = diff([p.StimI, length(sig)]);
-
-    for i = 1:numel(p.StimI)
-        data = sig((1:artifactNSamples(i)) + p.StimI(i) - 1);
-        data = pyrunfile('wrapper.py', 'data', data=data, tau=p.SALPA.tau, thresh=p.SALPA.thresh);
-
-        sig((1:artifactNSamples(i)) + p.StimI(i) - 1) = double(data);
+    if iscolumn(stimIdxs)
+        stimIdxs = stimIdxs';
     end
 
-    sig(isnan(sig)) = 0;
+    stimIdxs = [1, stimIdxs];
+    artifactNSamples = diff([stimIdxs, length(output)]);
 
-    if returnColumn
-        sig = sig';
+    for idx = 1:numel(stimIdxs)
+        data = output((1:artifactNSamples(idx)) + stimIdxs(idx) - 1);
+        data = pyrunfile('wrapper.py', 'data', data=data, ...
+            tau=tau, rail1=rail1, rail2=rail2, thresh=thresh, ...
+            t_blankdepeg=tBlankDepeg, t_ahead=tAhead, ...
+            t_chi2=tChi2, t_forcepeg=tForcePeg);
+
+        output((1:artifactNSamples(idx)) + stimIdxs(idx) - 1) = double(data);
+    end
+
+    if ~hasNan
+        output(isnan(output)) = 0;
+    end
+
+    if iscolumn(signal)
+        output = output';
     end
 end
--- wrapper.py
+++ wrapper.py
@@ -4,4 +4,36 @@ import numpy as np
 
 data = np.array(data)
 tau = int(tau)
-data = python_salpa.salpa(data, tau, thresh=thresh)
+
+kwargs = {}
+
+rail1 = np.array(rail1)
+rail2 = np.array(rail2)
+thresh = np.array(thresh)
+t_blankdepeg = np.array(t_blankdepeg)
+t_ahead = np.array(t_ahead)
+t_chi2 = np.array(t_chi2)
+t_forcepeg = np.array(t_forcepeg)
+
+if np.any(rail1):
+    kwargs['rail1'] = rail1
+
+if np.any(rail2):
+    kwargs['rail2'] = rail2
+
+if np.any(thresh):
+    kwargs['thresh'] = thresh
+
+if np.any(t_blankdepeg):
+    kwargs['t_blankdepeg'] = int(t_blankdepeg)
+
+if np.any(t_ahead):
+    kwargs['t_ahead'] = int(t_ahead)
+
+if np.any(t_chi2):
+    kwargs['t_chi2'] = int(t_chi2)
+
+if np.any(t_forcepeg):
+    kwargs['t_forcepeg'] = int(t_forcepeg)
+
+data = python_salpa.salpa(data, tau, **kwargs)
